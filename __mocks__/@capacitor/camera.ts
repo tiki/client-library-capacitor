a55e9a2a08a557270dab4a44d6e41e2f7d5d016c
Miro Benicio <mirodantas27@gmail.com>
2024-02-14 15:52:48 -0300

    wip: type error

--- __mocks__/@capacitor/camera.ts
+++ __mocks__/@capacitor/camera.ts
@@ -1,6 +1,11 @@
-import type { CameraPlugin } from "@capacitor/camera";
+import type { CameraPlugin, CameraResultType, CameraSource } from "@capacitor/camera";
 
 export const Camera: CameraPlugin = jest.createMockFromModule('@capacitor/camera')
+
+export const mockCameraResultType: CameraResultType = jest.createMockFromModule('@capacitor/camera') as CameraResultType
+
+export const mockCameraSource: CameraSource = jest.createMockFromModule('@capacitor/camera') as CameraSource
+
 
 Camera.requestPermissions = () => new Promise((resolve) => resolve({
   camera: 'granted',
