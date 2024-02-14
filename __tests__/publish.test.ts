e679c48b0499204de316b96ac22d9045501fd369
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2024-02-14 13:36:24 -0300

    test: setup

--- __tests__/publish.test.ts
+++ __tests__/publish.test.ts
@@ -1,12 +1,13 @@
+import type { Photo } from '@capacitor/camera';
+
 import Capture from '../src/Capture/index'
-import { Photo } from '@capacitor/camera';
 
 const capture = new Capture()
 
 const photo: Photo = {base64String: 'base64String', path: 'image/png', format: 'png', saved: true}
 
 describe('Take Photo mock function', () => {
-    test('returns a promise resolving to a Photo object', () => {
-     expect(capture.publish([photo])).toBeDefined()
-    });
+  test('returns a promise resolving to a Photo object', () => {
+    expect(capture.publish([photo])).toBeDefined()
   });
+});
--- __tests__/scan.test.ts
+++ __tests__/scan.test.ts
@@ -1,9 +1,12 @@
-import { Camera } from '../_mocks_/@capacitor/camera';
+import { Camera } from '@capacitor/camera';
+
+import { TikiClient } from '../';
 
 describe('Take Photo mock function', () => {
   test('returns a promise resolving to a Photo object', () => {
     // Assuming fetchPhoto returns a Promise that resolves to a Photo object
+    TikiClient.scan()
     expect(Camera.requestPermissions()).toBeDefined();
-    expect(Camera.getPhoto()).toBeDefined();
+    // expect(Camera.getPhoto()).toBeDefined();
   });
 });
